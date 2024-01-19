56b32eff458b65045ff10b5a08cbbb31dc983809
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-01-19 14:59:53 +0800

    Handled Nav visibility when scrolling + Implemented a scroll to top button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import Navlinks from "./Navlinks"
 import Socials from "./SocialLinks"
 import { motion } from "framer-motion"
 import Logo from "./Logo"
+import { FaArrowUp } from "react-icons/fa"
 
 const variants = {
   closed: {
@@ -59,14 +60,24 @@ const dimmer = {
     pointerEvents: "auto",
   },
 }
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  })
+}
+
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
   const [screen, setScreen] = useState("")
   const [activeLink, setActiveLink] = useState(null)
+  const [scrollDown, setScrollDown] = useState(false)
+  const scrollerTop = useRef(null)
   const indicator = useRef(null)
 
   const handleLinkIndicator = target => {
-    if (indicator.current && screen >= 1023) {
+    if (indicator.current && screen >= 800) {
       indicator.current.style.left = target.offsetLeft + "px"
       indicator.current.style.width = target.offsetWidth + "px"
     }
@@ -86,8 +97,40 @@ export default function Navbar() {
     }
   }, [])
 
+  // Scroll Handler
   useEffect(() => {
-    if (screen >= 1023) {
+    let prevScroll = window.scrollY
+
+    const handleScroll = () => {
+      const currentScroll = window.scrollY
+
+      if (currentScroll > prevScroll) {
+        console.log(currentScroll)
+        setScrollDown(true)
+      } else {
+        setScrollDown(false)
+      }
+
+      prevScroll = currentScroll
+
+      if (currentScroll < 650) {
+        scrollerTop.current.classList.add("opacity-0", "translate-y-full")
+        scrollerTop.current.classList.remove("opacity-50", "translate-y-0")
+      } else {
+        scrollerTop.current.classList.add("opacity-50", "translate-y-0")
+        scrollerTop.current.classList.remove("opacity-0", "translate-y-full")
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (screen >= 800) {
       setIsOpen(false)
       document.body.style.overflow = "auto"
     } else {
@@ -97,7 +140,7 @@ export default function Navbar() {
 
   const handleActiveLink = index => {
     setActiveLink(index)
-    screen <= 1023 && setIsOpen(isOpen => !isOpen)
+    screen <= 800 && setIsOpen(isOpen => !isOpen)
   }
 
   return (
@@ -109,7 +152,11 @@ export default function Navbar() {
         animate={isOpen ? "active" : "inactive"}
         onClick={() => setIsOpen(isOpen => !isOpen)}
       />
-      <nav className="main-nav">
+      <nav
+        className={`main-nav ${
+          scrollDown ? "-translate-y-full" : "-translate-y-0"
+        }`}
+      >
         <div className="flex md:hidden items-center">
           <motion.button
             variants={menuToggler}
@@ -192,6 +239,15 @@ export default function Navbar() {
           </ul>
         </section>
       </motion.aside>
+      <button
+        onClick={scrollToTop}
+        ref={scrollerTop}
+        className={`fixed right-3 bottom-3 z-[80]  w-8 aspect-square opacity-0 translate-y-full text-secondary rounded-full outline outline-secondary outline-2 grid place-items-center transition-all duration-[.3s] ease-in-out hover:opacity-100 hover:bg-primary`}
+      >
+        <FaArrowUp
+          style={{ width: "100%", height: "100%", padding: ".5rem" }}
+        />
+      </button>
     </>
   )
 }
